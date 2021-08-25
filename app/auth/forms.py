a087bc8720449f13825a6467d8f8be364c266777
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-25 21:53:26 +0300

    create a blueprint instance auth and logout function for authentication

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -17,10 +17,16 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(email = data_field.data).first():
             raise ValidationError('There is an account with that email')
 
-    
+
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
 
-            
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+
 
