0d6760a836106d8d6b2b9c7e89fabd5d2341ecda
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-26 20:50:48 +0300

    adds config file

--- config.py
+++ config.py
@@ -1,2 +1,51 @@
+import os
+class Config:
+    '''
+    This is the general configuration parent class
+    '''
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    SECRET_KEY='987654321!'
 
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDER_EMAIL = 'wambuafred13@gmail.com'
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+           SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://','postgresql://',1)
+    
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:12345@localhost/blog"   
+
+
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
     
