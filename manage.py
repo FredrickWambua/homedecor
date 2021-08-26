d0ca7fb9648185e2d6ecad9b7189768e83c8fcf6
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-26 20:41:52 +0300

    creates app factory

---
+++ manage.py
@@ -0,0 +1,21 @@
+import os
+from app import create_app, db
+from app.models import Comment, User, Pitch
+from flask_script import Manager, Server
+from flask_migrate import Migrate
+
+# creating an app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+migrate = Migrate(app,db)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch, Comment = Comment)
+
+if __name__ == '__main__':
+    manager.run()
